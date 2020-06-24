69ceffae1c5a4e81dfe0159eda2976ac86e84cc3
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2020-06-24 04:34:28 +0300

    back-end'den front-end'e bence çok iktel bir yöntemle veri geçiriyorum ileride burayı daha güzelleştirebilirsem iyi olur

--- girisYap.php
+++ girisYap.php
@@ -7,6 +7,16 @@
         // ^ bu kısmı girip yap sayfası ile işim bittikten sonra açıcam
     }
 ?>
+<?php if(isset($_SESSION["username"])): // php ile react'ı birleştirmek bana böyle çözümler üretmeye zorladı ?>
+    <script>
+        <?php echo "const username='".$_SESSION["username"]."'"; ?>
+    </script>
+<?php else: ?>
+    <script>
+        <?php echo "const username=undefined"; ?>
+    </script>
+<?php endif; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
