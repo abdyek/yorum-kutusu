eba14a0efd73a759f725fa3757cfd51495134712
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2020-06-27 14:02:10 +0300

    üye olma sayfası sorunsuz görünüyor

--- signUp.js
+++ signUp.js
@@ -34,25 +34,187 @@ var App = function (_React$Component) {
 var Content = function (_React$Component2) {
     _inherits(Content, _React$Component2);
 
-    function Content() {
+    function Content(props) {
         _classCallCheck(this, Content);
 
-        return _possibleConstructorReturn(this, (Content.__proto__ || Object.getPrototypeOf(Content)).apply(this, arguments));
+        var _this2 = _possibleConstructorReturn(this, (Content.__proto__ || Object.getPrototypeOf(Content)).call(this, props));
+
+        _this2.state = {
+            form: "normal",
+            emailText: "",
+            usernameText: "",
+            passwordText: "",
+            passwordVerificationText: "",
+            /*
+            error:[
+                "Kullanıcı adı hali hazırda kullanılmakta",
+                "E-mail hali hazırda kullanılmakta",
+                "Şifre çok kısa (en az 6 karakter)",
+                "Şifre en az 1 rakam ve 1 harf içermelidir"
+            ]
+            */
+            error: null
+        };
+        _this2.signUpClick = _this2.signUpClick.bind(_this2);
+        _this2.changeEmail = _this2.changeEmail.bind(_this2);
+        _this2.changeUsername = _this2.changeUsername.bind(_this2);
+        _this2.changePassword = _this2.changePassword.bind(_this2);
+        _this2.changePasswordVerification = _this2.changePasswordVerification.bind(_this2);
+        return _this2;
     }
 
     _createClass(Content, [{
+        key: "signUpClick",
+        value: function signUpClick(e) {
+            if (this.state.passwordText != this.state.passwordVerificationText) {
+                this.setState({
+                    error: ["Parola tekrarı ile uyumlu değil!"]
+                });
+            } else {
+                // API'ye gönderme işini buraya yapıyoruz
+                // this.state.error 'u null a eşitleyince hata mesajı görünmüyor,
+                // API'den gelen error'un valuesunu doğrudan this.state.error'a eşitle
+                // eğer başarılı bir şekilde hesap oluşturulmuşsa giriş sayfasına yönlendirip orada "üyelik işleminiz başarılı" diye bir mesaj verilebilir
+            }
+            e.preventDefault();
+        }
+    }, {
+        key: "changeEmail",
+        value: function changeEmail(e) {
+            this.setState({
+                emailText: e.target.value
+            });
+        }
+    }, {
+        key: "changeUsername",
+        value: function changeUsername(e) {
+            this.setState({
+                usernameText: e.target.value
+            });
+        }
+    }, {
+        key: "changePassword",
+        value: function changePassword(e) {
+            this.setState({
+                passwordText: e.target.value
+            });
+        }
+    }, {
+        key: "changePasswordVerification",
+        value: function changePasswordVerification(e) {
+            this.setState({
+                passwordVerificationText: e.target.value
+            });
+        }
+    }, {
         key: "render",
         value: function render() {
+            if (this.state.error) {
+                this.errorMessageContent = [];
+                for (var i = 0; i < this.state.error.length; i++) {
+                    this.errorMessageContent.push(React.createElement(
+                        "li",
+                        { key: i },
+                        this.state.error[i]
+                    ));
+                }
+            }
             return React.createElement(
                 "div",
                 null,
                 React.createElement(
                     Row,
-                    { size: "one" },
+                    { size: "sixteen" },
+                    React.createElement(WideColumn, { size: "four" }),
                     React.createElement(
-                        Column,
-                        null,
-                        "buraya giri\u015F yap \u015Feyleri gelecek"
+                        WideColumn,
+                        { size: "eight" },
+                        React.createElement(H, { type: "1", textAlign: "center", text: "\xDCye Ol" }),
+                        React.createElement(
+                            "form",
+                            { className: "ui form" },
+                            React.createElement(
+                                "div",
+                                { className: "field" },
+                                React.createElement(
+                                    "label",
+                                    null,
+                                    "E-posta"
+                                ),
+                                React.createElement("input", { type: "text", name: "id", placeholder: "e-posta", value: this.state.emailText, onChange: this.changeEmail })
+                            ),
+                            React.createElement(
+                                "div",
+                                { className: "field" },
+                                React.createElement(
+                                    "label",
+                                    null,
+                                    "Kullan\u0131c\u0131 Ad\u0131"
+                                ),
+                                React.createElement("input", { type: "text", name: "id", placeholder: "kullan\u0131c\u0131 ad\u0131", value: this.state.usernameText, onChange: this.changeUsername })
+                            ),
+                            React.createElement(
+                                "div",
+                                { className: "field" },
+                                React.createElement(
+                                    "label",
+                                    null,
+                                    "Parola"
+                                ),
+                                React.createElement("input", { type: "password", name: "password", placeholder: "parola", value: this.state.passwordText, onChange: this.changePassword })
+                            ),
+                            React.createElement(
+                                "div",
+                                { className: "field" },
+                                React.createElement(
+                                    "label",
+                                    null,
+                                    "Parola Tekrar"
+                                ),
+                                React.createElement("input", { type: "password", name: "password", placeholder: "parola tekrar", value: this.state.passwordVerificationText, onChange: this.changePasswordVerification })
+                            )
+                        )
+                    )
+                ),
+                React.createElement(
+                    Row,
+                    { size: "sixteen" },
+                    React.createElement(WideColumn, { size: "four" }),
+                    React.createElement(
+                        WideColumn,
+                        { size: "eight" },
+                        this.state.error ? React.createElement(
+                            "div",
+                            { className: "ui error message" },
+                            React.createElement(
+                                "div",
+                                { className: "header" },
+                                "\xDCyelik i\xE7in bu ko\u015Fullar\u0131 sa\u011Flaman\u0131z gerekiyor"
+                            ),
+                            React.createElement(
+                                "ul",
+                                { className: "list" },
+                                this.errorMessageContent
+                            )
+                        ) : ""
+                    )
+                ),
+                React.createElement(
+                    Row,
+                    { size: "sixteen" },
+                    React.createElement(WideColumn, { size: "four" }),
+                    React.createElement(
+                        WideColumn,
+                        { size: "eight" },
+                        React.createElement(
+                            FloatRight,
+                            null,
+                            React.createElement(
+                                "button",
+                                { className: "ui primary button", type: "submit", onClick: this.signUpClick },
+                                "\xDCye Ol"
+                            )
+                        )
                     )
                 )
             );
--- src/signUp.js
+++ src/signUp.js
@@ -11,13 +11,123 @@ class App extends React.Component {
 }
 
 class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            form:"normal",
+            emailText:"",
+            usernameText:"",
+            passwordText:"",
+            passwordVerificationText:"",
+            /*
+            error:[
+                "Kullanıcı adı hali hazırda kullanılmakta",
+                "E-mail hali hazırda kullanılmakta",
+                "Şifre çok kısa (en az 6 karakter)",
+                "Şifre en az 1 rakam ve 1 harf içermelidir"
+            ]
+            */
+            error:null
+        }
+        this.signUpClick = this.signUpClick.bind(this);
+        this.changeEmail = this.changeEmail.bind(this);
+        this.changeUsername = this.changeUsername.bind(this);
+        this.changePassword = this.changePassword.bind(this);
+        this.changePasswordVerification = this.changePasswordVerification.bind(this);
+    }
+    signUpClick(e) {
+        if(this.state.passwordText!=this.state.passwordVerificationText) {
+            this.setState({
+                error:["Parola tekrarı ile uyumlu değil!"]
+            })
+        } else {
+            // API'ye gönderme işini buraya yapıyoruz
+            // this.state.error 'u null a eşitleyince hata mesajı görünmüyor,
+            // API'den gelen error'un valuesunu doğrudan this.state.error'a eşitle
+            // eğer başarılı bir şekilde hesap oluşturulmuşsa giriş sayfasına yönlendirip orada "üyelik işleminiz başarılı" diye bir mesaj verilebilir
+        }
+        e.preventDefault();
+    }
+    changeEmail(e) {
+        this.setState({
+            emailText:e.target.value
+        })
+    }
+    changeUsername(e) {
+        this.setState({
+            usernameText:e.target.value
+        })
+    }
+    changePassword(e) {
+        this.setState({
+            passwordText:e.target.value
+        })
+    }
+    changePasswordVerification(e) {
+        this.setState({
+            passwordVerificationText:e.target.value
+        })
+    }
     render() {
+        if(this.state.error) {
+            this.errorMessageContent = [];
+            for(let i=0;i<this.state.error.length;i++) {
+                this.errorMessageContent.push(
+                    <li key={i}>{this.state.error[i]}</li>
+                )
+            }
+        }
         return(
             <div>
-                <Row size="one">
-                    <Column>
-                        buraya giriş yap şeyleri gelecek
-                    </Column>
+                <Row size="sixteen">
+                    <WideColumn size="four"></WideColumn>
+                    <WideColumn size="eight">
+                        <H type="1" textAlign="center" text="Üye Ol"/>
+                        <form className="ui form">
+                            <div className="field">
+                                <label>E-posta</label>
+                                <input type="text" name="id" placeholder="e-posta" value={this.state.emailText} onChange={this.changeEmail}/>
+                            </div>
+                            <div className="field">
+                                <label>Kullanıcı Adı</label>
+                                <input type="text" name="id" placeholder="kullanıcı adı"value={this.state.usernameText} onChange={this.changeUsername} />
+                            </div>
+                            <div className="field">
+                                <label>Parola</label>
+                                <input type="password" name="password" placeholder="parola" value={this.state.passwordText} onChange={this.changePassword}/>
+                            </div>
+                            <div className="field">
+                                <label>Parola Tekrar</label>
+                                <input type="password" name="password" placeholder="parola tekrar" value={this.state.passwordVerificationText} onChange={this.changePasswordVerification}/>
+                            </div>
+                        </form>
+                    </WideColumn>
+                </Row>
+                <Row size="sixteen">
+                    <WideColumn size="four"></WideColumn>
+                    <WideColumn size="eight">
+                        {
+                            this.state.error?
+                            <div className="ui error message">
+                                <div className="header">
+                                    Üyelik için bu koşulları sağlamanız gerekiyor
+                                </div>
+                                <ul className="list">
+                                    {this.errorMessageContent}
+                                </ul>
+                            </div> 
+                            :
+                            ""
+                        }
+                    </WideColumn>
+                </Row>
+                <Row size="sixteen">
+                    <WideColumn size="four"></WideColumn>
+                    <WideColumn size="eight">
+                        <FloatRight>
+                            <button className="ui primary button" type="submit" onClick={this.signUpClick}>Üye Ol</button>
+                        </FloatRight>
+                    </WideColumn>
                 </Row>
             </div>
         )
