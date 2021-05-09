804d7e6cd7821f8952fd6746040a9cd43a7d8cb9
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2021-05-09 21:44:57 +0300

    Bir takım meta etiketleri ve robots.txt

--- src/View/Php/Template.php
+++ src/View/Php/Template.php
@@ -10,6 +10,13 @@
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
+    <meta name="language" content="tr">
+    <meta name="keywords" content="yorum, yorumlama, ürün, yorum kutusu"/>
+    <meta name="description" content="Ürün yorumlama üzerine bir web sitesi"/>
+    <meta name="copyright"content="yorumkutusu.com">
+    <meta name="designer" content="Yunus Emre Bulut">
+    <meta name="owner" content="Yunus Emre Bulut">
+    <meta name="url" content="https://www.yorumkutusu.com">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle ?></title>
     <link rel="stylesheet" href="<?php echo Config::SITEURL ?>public/css/style.css">
