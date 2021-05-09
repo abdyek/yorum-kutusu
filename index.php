8d0485ef111d096edc3e63c9d7f96fced538f89f
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2021-05-09 22:24:43 +0300

    Sorunsuz bir şekilde yönlendirme yapıyor şu an yorumkutusu.com 'dan www.yoru.. a

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off" || $_SERVER['HTTP_HOST']==='yorumkutusu.com') {
     $location = 'https://www.' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
